625714a3c29240ca8c8fac94236530454449de45
crazybox521 <yunshu233@qq.com>
2022-04-21 00:00:28 +0800

    prod

--- src/service/get.js
+++ src/service/get.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Vue from 'vue'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.timeout = 10000
+axios.defaults.baseURL="http://47.102.159.133/"
 /* 项目大部分为get请求，封装一个统一的处理错误、传递参数和url的get方法 */
 export default (url, params) => axios.get(url, { params })
     .then(res => res.data)
